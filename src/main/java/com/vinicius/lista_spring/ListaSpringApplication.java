3a0ae12d7bae7eb8742e97ab4c8498cb0b3edeee
Vinicius Maciel <vinissmaciel@gmail.com>
2024-12-07 17:20:39 -0300

    first commit

---
+++ src/main/java/com/vinicius/lista_spring/ListaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.lista_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicius/lista_spring/ListaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.lista_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
